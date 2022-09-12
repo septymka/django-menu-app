2fa2c550ffea0592cc5ee7ffbe5776c7df53dbab
jsara <jadwiga.sarata@gmail.com>
2022-09-12 14:31:50 +0200

    add register user function

---
+++ users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from users.forms import RegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            msg = f'Welcome {username}, your account is created.'
+            messages.success(request, msg)
+            return redirect('food:index')
+    else:
+        form = RegisterForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
