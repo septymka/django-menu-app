70bf785fd4c31176097a21fd4c527e22b152c64f
jsara <jadwiga.sarata@gmail.com>
2022-09-20 09:23:23 +0200

    replace function based view by class based view for index and detail pages

--- food/urls.py
+++ food/urls.py
@@ -6,9 +6,8 @@ from food import views
 app_name = 'food'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail'),
-    # add items
+    path('', views.IndexClassView.as_view(), name='index'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     path('add/', views.create_item, name='create_item'),
     path('update/<int:item_id>/', views.update_item, name='update_item'),
     path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
--- food/views.py
+++ food/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from food.models import Item
 from food.forms import ItemForm
 
 
-def index(request):
-    items = Item.objects.all()
-    context = {'item_list': items,}
-    return render(request, 'food/index.html', context)
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {'item': item,}
-    return render(request, 'food/detail.html', context)
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
+
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
 
 def create_item(request):
     form = ItemForm(request.POST or None)
